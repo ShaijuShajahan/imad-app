4e60582468025e9db306fb7f1fbbb8ea88e7be7a
ShaijuShajahan <shaijushajahan@hotmail.com>
2017-08-12 17:32:30 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,13 @@
-console.log('Loaded!');
+//counter code
+var counter=0;
+var button= document.getElementById("counter");
+button.onclick= function() {
+    
+    
+    
+    //render the variable
+    counter=counter+1;
+    var span=document.getElementById("counter");
+    span.innerhtml=counter.tostring();
+    
+}
